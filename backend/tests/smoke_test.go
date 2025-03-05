3ae37c55f129915fa546138462579376814fdd43
khwong-c64 <kin.hin.wong.c@gmail.com>
2025-03-05 23:36:58 +0000

    test: Health Checker + Test Suite

--- backend/tests/smoke_test.go
+++ backend/tests/smoke_test.go
@@ -2,13 +2,14 @@ package tests
 
 import (
 	"flag"
+	"net/http"
 	"os"
 	"testing"
 
 	"github.com/cristalhq/aconfig"
 	"github.com/juju/errors"
 	"github.com/samber/do"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/khwong-c/wtcode/config"
 	"github.com/khwong-c/wtcode/server"
@@ -29,10 +30,37 @@ func SkipConfigFlags(*do.Injector) (*config.Config, error) {
 	return &newCfg, nil
 }
 
-func TestBootServer(t *testing.T) {
-	injector := di.CreateInjector(false, false)
-	di.InvokeOrProvide(injector, SkipConfigFlags)
-	assert.NotPanics(t, func() {
-		di.InvokeOrProvide(injector, server.CreateServer)
-	})
+type SmokeTestSuite struct {
+	suite.Suite
+	injector *do.Injector
+	svr      *server.Server
+}
+
+func TestSmokeTests(t *testing.T) {
+	suite.Run(t, new(SmokeTestSuite))
+}
+
+func (ts *SmokeTestSuite) SetupSuite() {
+	ts.injector = di.CreateInjector(false, false)
+	di.InvokeOrProvide(ts.injector, SkipConfigFlags)
+
+	if !ts.NotPanics(func() {
+		ts.svr = di.InvokeOrProvide(ts.injector, server.CreateServer)
+	}) {
+		ts.FailNow("Failed to create server")
+	}
+}
+
+func (ts *SmokeTestSuite) TearDownSuite() {
+	ts.NoError(ts.injector.Shutdown())
+}
+
+func (ts *SmokeTestSuite) TestHealthEndpoint() {
+	ts.HTTPBodyContains(
+		ts.svr.Handler.ServeHTTP,
+		http.MethodGet,
+		"/health",
+		nil,
+		".",
+	)
 }
