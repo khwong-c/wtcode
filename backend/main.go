4e3206c5b281caab0063d79f492c839435151614
khwong-c64 <kin.hin.wong.c@gmail.com>
2025-03-20 00:24:12 +0000

    feat: Creating Server Stack, Listen and Serve

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/khwong-c/wtcode/server"
 	"github.com/khwong-c/wtcode/tooling/di"
 	"github.com/khwong-c/wtcode/tooling/log"
@@ -11,6 +13,11 @@ var logger = log.NewLogger("main")
 func main() {
 	injector := di.CreateInjector(true, false)
 	svr := di.InvokeOrProvide(injector, server.CreateServer)
-	logger.Info("Server created", "addr", svr.Addr, "cfg", svr.Config)
-	_ = injector.Shutdown()
+	logger.Info("Server created", "addr", svr.Addr)
+
+	svr.Serve()
+	err := injector.ShutdownOnSignals(os.Interrupt, os.Kill)
+	if err != nil {
+		logger.Error("Injector shutdown error", "err", err)
+	}
 }
